f6f515a5a3c6f821b0cfe64bf5dcb307e66560a1
bruno dias <diasbruno00@gmail.com>
2025-08-06 12:43:20 -0300

    first commit

---
+++ src/main/java/br/edu/ufop/web/carteira/investimentos/carteiraDeinvestimentos/CarteiraDeinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufop.web.carteira.investimentos.carteiraDeinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraDeinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraDeinvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufop/web/carteira/investimentos/carteiraDeinvestimentos/CarteiraDeinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufop.web.carteira.investimentos.carteiraDeinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraDeinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
